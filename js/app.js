74666666896aa49d7664979aa9155d116d786f73
dipsana24 <adhikaridipsana24@gmail.com>
2026-02-08 17:38:18 +0545

    Remove duplicate setDefaultDate function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+(function setDefaultDate() {
+  const today = new Date().toISOString().split("T")[0];
+  if (dateInput) {
+    dateInput.value = today;
+  }
+})();
+
+(function setDefaultDate() {
+  const today = new Date().toISOString().split("T")[0];
+  if (dateInput) {
+    dateInput.value = today;
+  }
+})();
 // form submit = add / update
 form.addEventListener("submit", handleSubmit);
 
